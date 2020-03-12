7f5b19111f888f12b962c2daa8237540910981b9
Richard Morrison <richard@rmorrison.net>
2020-03-12 17:13:38 +0000

    TDD.  Add a column and see how migrations are auto created. ./manage.py sqlmigrate cups 0003 is informative

--- cups/models.py
+++ cups/models.py
@@ -5,6 +5,7 @@ class Trophy(models.Model):
         verbose_name_plural = "trophies"
 
     name = models.CharField(max_length=100)
+    weight = models.PositiveIntegerField(help_text="in g")
 
     def __str__(self):
         return self.name
--- cups/tests.py
+++ cups/tests.py
@@ -3,14 +3,14 @@ from .models import Trophy
 
 class TrophiesTestCase(TestCase):
     def test_trophy_creation(self):
-        trophy = Trophy(name="European Cup")
+        trophy = Trophy(name="European Cup", weight=5000)
         trophy.save()
 
         # Django wraps every test in a transaction
         self.assertEqual(1, Trophy.objects.count())
 
     def test_trophy_creation_independent(self):
-        trophy = Trophy(name="Premier League")
+        trophy = Trophy(name="Premier League", weight=4000)
         trophy.save()
 
         # No inter-dependent tests here
