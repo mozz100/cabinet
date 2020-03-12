8ead94a96217c3447989b8e1a9d40215076a6282
Richard Morrison <richard@rmorrison.net>
2020-03-12 17:06:26 +0000

    Admin site (will need to create local db and createsuperuser - note password defaults). Remember to makemigrations

--- cups/models.py
+++ cups/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Trophy(models.Model):
+    class Meta:
+        verbose_name_plural = "trophies"
+
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
