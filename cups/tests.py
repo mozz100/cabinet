bb33bf7d63d38d189b7dd37e02a02ece20517b63
Richard Morrison <richard@rmorrison.net>
2020-03-12 16:55:13 +0000

    TDD, add a model and run ./manage.py makemigrations

--- cups/tests.py
+++ cups/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Trophy
 
-# Create your tests here.
+class TrophiesTestCase(TestCase):
+    def test_trophy_creation(self):
+        trophy = Trophy(name="European Cup")
+        trophy.save()
+
+        # Django wraps every test in a transaction
+        self.assertEqual(1, Trophy.objects.count())
+
+    def test_trophy_creation_independent(self):
+        trophy = Trophy(name="Premier League")
+        trophy.save()
+
+        # No inter-dependent tests here
+        self.assertEqual(1, Trophy.objects.count())
